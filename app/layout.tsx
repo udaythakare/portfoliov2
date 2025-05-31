4716cf1855bc82b3b465414aed877c4f66212f11
yuxdi <udaythakare592004@gmail.com>
2025-05-31 16:43:47 +0530

    removed footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
-};
+// export const metadata = {
+//   title: "Ricardo | Personal Portfolio",
+//   description: "Ricardo is a full-stack developer with 8 years of experience.",
+// };
 
 export default function RootLayout({
   children,
@@ -31,7 +31,7 @@ export default function RootLayout({
           <ActiveSectionContextProvider>
             <Header />
             {children}
-            <Footer />
+            {/* <Footer /> */}
 
             <Toaster position="top-right" />
             <ThemeSwitch />
